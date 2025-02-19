6a823b9187ddda889b164e20dd0bd5ca6b657835
ukeruk <58307060+ukeruk@users.noreply.github.com>
2025-02-19 16:48:16 +0200

    New attempt at world gen with perlin noise

---
+++ script.js
@@ -0,0 +1,14 @@
+const size = 500;
+var ns;
+noise.seed(Math.random());
+
+height_map = [];
+
+for (let x = 0; x < size; x++) {
+    height_map[x] = [];
+    for (let y = 0; y < size; y++) {
+        height_map[x][y] = 
+        
+    }
+    
+}
